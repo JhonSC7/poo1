a0c694d34a688b851ce8b1d54b87fc3b08a3d320
jhon Sabogal Cristancho <jesabogalc@gmail.com>
2025-04-11 13:41:38 -0500

    un mensaje

--- src/Funcion.java
+++ src/Funcion.java
@@ -1,13 +1,34 @@
 public class Funcion {
     //Atributos para nuestro ejemplo
-    int lado;
     double radio;
+    int altura;
+    int lado;
     short base;
     //Metodos para nuestro ejemplo
 
-    public short calcularCubo(short base)
-    {
-        return (short) (base*base*base);
+    public short calcularCubo(short base) {
+        return (short) (base * base * base);
     }
 
+    public int calcularArea(int lado) {
+        return (lado * lado);
+    }
+
+    public int calcularAreatriangulo(int base, int lado) {
+        return (base * altura / 2);
+    }
+
+    public int calcularVolumenCubo(int lado) {
+        return (lado * lado * lado);
+    }
+
+    public double calcularVolumenEsfera(double radio){
+        return (4/3*Math.PI*radio*radio*radio);
+    }
+    public double calcularPerimetroCirculo( double radio){
+        return (2*Math.PI*radio);
+    }
+    public int calcularPerimetroPentagonal(int lado){
+        return(5*lado);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,28 @@ public class Main {
         Scanner leer = new Scanner(System.in);
         //Crear un objeto de tipo funciono llamado algoraro
         Funcion algoraro = new Funcion();
+        algoraro.altura=8;
         algoraro.base=10;
+        algoraro.lado=15;
+        algoraro.radio=5;
+
         short cubo = algoraro.calcularCubo(algoraro.base);
+        int area = algoraro.calcularArea(algoraro.lado);
+        int areatriangulo = algoraro.calcularAreatriangulo(algoraro.base, algoraro.altura);
+        int volumenCubo =  algoraro.calcularVolumenCubo(algoraro.lado);
+        double volumenEsfera= algoraro.calcularVolumenEsfera(algoraro.radio);
+        double perimetroCirculo= algoraro.calcularPerimetroCirculo(algoraro.radio);
+        int perimetroPentagonal= algoraro.calcularPerimetroPentagonal(algoraro.lado);
 
-        System.out.println(cubo);
-
+        //mostar valores de cada calculo
+        System.out.println("el cubo es: " +cubo);
+        System.out.println("el area es: " +area);
+        System.out.println("el area del triangulo es :" +areatriangulo);
+        System.out.println("el volumen del cubo es: " +volumenCubo);
+        System.out.println("el volumen de la esfera es: " +volumenEsfera);
+        System.out.println("el perimetro del circulo es : " +perimetroCirculo);
+        System.out.println("el perimetro pentagonal es : " +perimetroPentagonal);
 
     }
-    public static String person(String name, int year){
-        return (name+" you are "+year).toUpperCase();
-    }
+
 }
